c2190fca8fa0ab61e8e25922aabb89a40ec8ac71
Hiralprajapti <hiralprajapati086@gmail.com>
2024-09-17 11:39:20 +0000

    continaier congi

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 1414235
+let accountEmail = "hitesh@gmail.com"
+var accountPaasword = "12345"
+accountCity = "ladol"
+accountState;
+
+// accountId = 2   cont ni value ne change na kari saky
+
+accountEmail = "hc@hc.com"
+accountPaasword = "24861545"
+accountCity = "vijapur"
+
+//console.log(accountId);
+
+console.table([accountId,accountEmail,accountPaasword, accountCity, accountState]);
+
+
+/*
+prefer not use var
+because of isuue in block scope and function scope
+*/
+
+
+
+
+
