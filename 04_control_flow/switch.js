bdeb4699354561404fb8f8bb278bcb1af9f3ad2e
Hiralprajapti <hiralprajapati086@gmail.com>
2024-09-25 10:46:24 +0000

    loops running

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "mar":
+        console.log("march");
+         break; 
+    case "apr":
+        console.log("april");
+        break;      
+
+    default:
+        console.log("default case match");
+        break;
+}
